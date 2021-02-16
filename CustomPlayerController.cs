32c5cc6917c75a25e0fcc9bc885dd722f670f37e
rurre <sergiuung@msn.com>
2021-02-16 18:00:19 +0200

    Added dashing and tuned ground pound

--- CustomPlayerController.cs
+++ CustomPlayerController.cs
@@ -8,35 +8,50 @@ using VRC.Udon;
 public class CustomPlayerController : UdonSharpBehaviour
 {
     // Debug
-    public Text currentStateDebugText;
-    public Text nextStateDebugText;
-    public Text jumpDebugText;
+    public Text currentStateText;
+    public Text nextStateText;
+    public Text jumpText;
+    public Text dashText;
+    public Text groundedText;
+    public Text groundedGraceText;
+    public Text distanceToGroundText;
 
     // Stats
     public float slideVelocity = 10;
     public float groundPoundVelocity = 15;
     public float jumpImpulseOverride = 10;
 
+    public float dashVelocity = 20;
+    public float dashTime = 0.13f;
+    public float dashJuiceMax = 3;
+    public float dashRegenRate = 0.3f;
+    public float ignoreNotGroundedMaxTime = 1;
+
     public float groundPoundTooCloseDistance = 0.1f;
     public LayerMask groundMask;
+
+    float distanceToGroundLast;
+    sbyte _tooCloseToGroundThisFrame = -1;
 
     // Input
     public KeyCode slideKey = KeyCode.LeftControl;
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.LeftShift;
 
     VRCPlayerApi player;
+    bool _playerGrounded;
+    float _timeNotGrounded;
 
     #region States
 
     /// <summary>
-    /// 0 - Idle,
-    /// 1 - Jumping,
-    /// 2 - Sliding,
-    /// 3 - GroundPound,
-    /// 4 - Dashing
+    /// Idle = 0,
+    /// Jumping = 1,
+    /// Sliding = 2,
+    /// GroundPound = 3,
+    /// Dashing = 4
     /// </summary>
     int state = 0;
-    int lastState = 0;
     int nextState = -1;
 
     string StateToString(int state)
@@ -66,13 +81,15 @@ public class CustomPlayerController : UdonSharpBehaviour
     {
         player = Networking.LocalPlayer;
         player.SetJumpImpulse(0);
+        dashJuice = dashJuiceMax;
     }
 
     #region Input
 
-    bool _isHoldingSlideKey = false;
-    bool _pressedSlide = false;
-    bool _pressedJump = false;
+    bool _isHoldingSlideKey;
+    bool _pressedSlide;
+    bool _pressedJump;
+    bool _pressedDash;
 
     void HandleInput()
     {
@@ -82,12 +99,21 @@ public class CustomPlayerController : UdonSharpBehaviour
             _pressedSlide = Input.GetKeyDown(slideKey);
         if(!_pressedJump)
             _pressedJump = Input.GetKeyDown(jumpKey);
+        if(!_pressedDash)
+            _pressedDash = Input.GetKeyDown(dashKey);
     }
 
-    void ConsumeInputs()
+    void ResetFrameVariables()
     {
         _pressedSlide = false;
         _pressedJump = false;
+        _pressedDash = false;
+        _tooCloseToGroundThisFrame = (sbyte)-1;
+    }
+
+    bool PressedDash()
+    {
+        return _pressedDash;
     }
 
     bool IsHoldingSlide()
@@ -112,7 +138,29 @@ public class CustomPlayerController : UdonSharpBehaviour
 
     #endregion
 
-    #region Updates
+    #region Debug
+
+    private void UpdateDebugText()
+    {
+        if(currentStateText)
+            currentStateText.text = StateToString(state);
+        if(nextStateText)
+            nextStateText.text = StateToString(nextState);
+        if(jumpText)
+            jumpText.text = PressedJump().ToString();
+        if(groundedText)
+            groundedText.text = IsGrounded().ToString();
+        if(groundedGraceText)
+            groundedGraceText.text = IsGroundedGrace().ToString();
+        if(dashText)
+            dashText.text = $"{dashJuice}/{dashJuiceMax}";
+        if(distanceToGroundText)
+            distanceToGroundText.text = distanceToGroundLast > -1 ? distanceToGroundLast.ToString() : "didn't hit";
+    }
+
+    #endregion
+
+    #region Updates and Checks
 
     void Update()
     {
@@ -121,13 +169,42 @@ public class CustomPlayerController : UdonSharpBehaviour
 
     void FixedUpdate()
     {
+        HandleGroundCheck();
+        RegenerateDash(Time.fixedDeltaTime);
         ExecuteMainStateMachine();
-        ConsumeInputs();
+        ResetFrameVariables();
+        UpdateDebugText();
     }
 
     #endregion
 
-    #region State Machines
+    #region Ground Checks
+
+    void HandleGroundCheck()
+    {
+        if(_playerGrounded = player.IsPlayerGrounded())
+        {
+            _timeNotGrounded = 0;
+            return;
+        }
+
+        if(_timeNotGrounded < ignoreNotGroundedMaxTime)
+            _timeNotGrounded += Time.fixedDeltaTime;
+    }
+
+    bool IsGroundedGrace()
+    {
+        return _playerGrounded || (_timeNotGrounded <= ignoreNotGroundedMaxTime);
+    }
+
+    bool IsGrounded()
+    {
+        return _playerGrounded;
+    }
+
+    #endregion
+
+    #region Main State Machines
 
     void ExecuteMainStateMachine()
     {
@@ -145,10 +222,12 @@ public class CustomPlayerController : UdonSharpBehaviour
                 case 3:
                     EnterGroundPound();
                     break;
+                case 4:
+                    EnterDash();
+                    break;
                 default:
                     break;
             }
-            lastState = state;
             state = nextState;
             nextState = -1;
         }
@@ -169,26 +248,35 @@ public class CustomPlayerController : UdonSharpBehaviour
                 case 3:
                     GroundPoundStateMachine();
                     break;
+                case 4:
+                    DashStateMachine();
+                    break;
                 default:
                     break;
             }
         }
-
-        if(currentStateDebugText)
-            currentStateDebugText.text = StateToString(state);
-        if(nextStateDebugText)
-            nextStateDebugText.text = StateToString(nextState);
-        if(jumpDebugText)
-            jumpDebugText.text = PressedJump().ToString();
     }
 
     void IdleStateMachine()
     {
-        if(PressedSlide() && CanStartSlide())
-            nextState = 2;
+        if(PressedSlide())
+        {
+            if(CanStartSlide() || TooCloseToPound())
+                nextState = 2;
+            else if(CanGroundPound())
+                nextState = 3;
+        }
         else if(PressedJump() && CanJump())
             nextState = 1;
+        else if(PressedDash() && CanStartDash())
+            nextState = 4;
     }
+
+    #endregion
+
+    #region Slide State
+
+    Vector3 slideDirection;
 
     void SlideStateMachine()
     {
@@ -198,35 +286,29 @@ public class CustomPlayerController : UdonSharpBehaviour
             return;
         }
 
-        if(IsHoldingSlide())
+        if(PressedDash() && CanStartDash())
+        {
+            ExitSlide(4);
+            return;
+        }
+
+        if(IsHoldingSlide()) //&& CanContinueSlide()) TODO: Add back
+        {
             ExecuteSlide();
-        else
-            ExitSlide(0);
+            return;
+        }
+
+        ExitSlide(0);
     }
-
-    void JumpStateMachine()
-    {
-        if(player.IsPlayerGrounded())
-            ExitJump(0);
-        else if(PressedGroundPound() && CanGroundPound())
-            ExitJump(3);
-    }
-
-    void GroundPoundStateMachine()
-    {
-        if(player.IsPlayerGrounded())
-            ExitGroundPound(0);
-    }
-
-    #endregion
-
-    #region Slide State
-
-    Vector3 slideDirection;
 
     bool CanStartSlide()
     {
-        return state == 0 && player.IsPlayerGrounded();
+        return state == 0 && _playerGrounded;
+    }
+
+    bool CanContinueSlide()
+    {
+        return IsGroundedGrace();
     }
 
     void EnterSlide()
@@ -241,7 +323,6 @@ public class CustomPlayerController : UdonSharpBehaviour
 
     void ExitSlide(int nextState)
     {
-        Debug.Log("Exitting slide, going to " + StateToString(nextState));
         this.nextState = nextState;
     }
 
@@ -249,9 +330,30 @@ public class CustomPlayerController : UdonSharpBehaviour
 
     #region Jump State
 
+    void JumpStateMachine()
+    {
+        if(IsGroundedGrace())
+        {
+            ExitJump(0);
+            return;
+        }
+
+        if(PressedGroundPound() && CanGroundPound())
+        {
+            ExitJump(3);
+            return;
+        }
+
+        if(PressedDash() && CanStartDash())
+        {
+            ExitJump(4);
+            return;
+        }
+    }
+
     bool CanJump()
     {
-        return player.IsPlayerGrounded();
+        return IsGrounded();
     }
 
     void EnterJump()
@@ -268,19 +370,31 @@ public class CustomPlayerController : UdonSharpBehaviour
 
     #region Ground Pound State
 
+    void GroundPoundStateMachine()
+    {
+        if(IsGroundedGrace())
+            ExitGroundPound(0);
+        else if(PressedDash() && CanStartDash())
+            ExitGroundPound(4);
+    }
+
+    bool TooCloseToPound()
+    {
+        if(_tooCloseToGroundThisFrame == -1)
+        {
+            Ray ray = new Ray(player.GetPosition(), Vector3.down);
+            RaycastHit hit;
+            bool wasHit = Physics.Raycast(ray, out hit, groundPoundTooCloseDistance, groundMask, QueryTriggerInteraction.UseGlobal);
+            distanceToGroundLast = hit.distance;
+            _tooCloseToGroundThisFrame = wasHit ? (sbyte)1 : (sbyte)0;
+        }
+
+        return _tooCloseToGroundThisFrame == 1;
+    }
+
     bool CanGroundPound()
     {
-        if(player.IsPlayerGrounded())
-            return false;
-
-        Ray ray = new Ray(player.GetPosition(), Vector3.down);
-        RaycastHit hit;
-        bool tooCloseToGround = Physics.Raycast(ray, out hit, groundPoundTooCloseDistance, groundMask, QueryTriggerInteraction.UseGlobal);
-        string name = hit.collider ? hit.collider.name : "nothing";
-        Debug.Log($"{tooCloseToGround}, hit {name}");
-        Debug.DrawRay(ray.origin, ray.direction, Color.yellow, 10);
-
-        return !tooCloseToGround;
+        return !IsGroundedGrace() && !TooCloseToPound();
     }
 
     void EnterGroundPound()
@@ -291,6 +405,70 @@ public class CustomPlayerController : UdonSharpBehaviour
     void ExitGroundPound(int nextState)
     {
         this.nextState = nextState;
+    }
+
+    #endregion
+
+    #region Dash State
+
+    float dashJuice = 0;
+    float dashTimeLeft = 0;
+    Vector3 dashDir;
+
+
+    bool CanStartDash()
+    {
+        if(dashJuice >= 1)
+        {
+            dashTimeLeft = dashTime;
+            return true;
+        }
+        return false;
+    }
+
+    void DashStateMachine()
+    {
+        if(dashTimeLeft <= 0)
+        {
+            ExitDash(0);
+            return;
+        }
+
+        player.SetVelocity(dashDir * dashVelocity);
+        dashTimeLeft -= Time.fixedDeltaTime;
+    }
+
+    void EnterDash()
+    {
+        dashJuice -= 1;
+        var vel = player.GetVelocity();
+        if(vel.x == 0 && vel.z == 0)
+        {
+            dashDir = player.GetRotation() * Vector3.forward;
+            return;
+        }
+
+        vel.y = 0;
+        dashDir = vel.normalized;
+    }
+
+    void ExitDash(int nextState)
+    {
+        this.nextState = nextState;
+        player.SetVelocity(Vector3.zero);
+    }
+
+    void RegenerateDash(float deltaTime)
+    {
+        // Can't regenerate while sliding or dashing
+        if(state == 2 || state == 4)
+            return;
+
+
+        if(dashJuice < dashJuiceMax)
+            dashJuice += deltaTime * dashRegenRate;
+        else
+            dashJuice = dashJuiceMax;
     }
 
     #endregion
